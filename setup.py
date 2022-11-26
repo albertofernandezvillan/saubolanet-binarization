cb714e6eefb033d65561cef4dbe36334c4e98659
Alberto Fernández Villán <fernandezvillan.alberto@gmail.com>
2022-11-26 13:52:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='sauvolanet',
-    version='0.0.7',
+    version='0.0.8',
     description='Sauvolanet binarization algorithm',
     long_description=long_description,
     long_description_content_type="text/markdown",
